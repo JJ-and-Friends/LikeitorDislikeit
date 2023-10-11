e9567b075d0c33a97ddf646aa51c1f5b92cbd6fc
Jardel - JD <mitchelljardel@gmail.com>
2023-10-11 01:34:49 +0000

    hope this doesn't break anything.........

--- App/views/rate.py
+++ App/views/rate.py
@@ -12,7 +12,38 @@ from App.controllers import (
 
 rate_views = Blueprint('rate_views', __name__, template_folder='../templates')
 
-@rate_views.route('rate/review', methods=['POST'])
+@rate_views.route('/rate/review', methods=['POST'])
 def add_rating():
-    pass
+    try:
+        # Get JSON data from the request
+        data = request.get_json()
+        # Extract the necessary parameters from the JSON data
+        sid = data.get('sid')
+        uid = data.get('uid')
+        title = data.get('title')
+        description = data.get('description')
 
+        # Call the controller function to add a review with the extracted data
+        success, message = add_review(sid, uid, title, description)
+
+        if success:
+            return jsonify({'message': message}), 201  # 201 Created status code for successful addition
+        else:
+            return jsonify({'error': message}), 400  # 400 Bad Request status code for errors
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # 500 Internal Server Error for unexpected exceptions
+
+@rate_views.route('/rate/get-reviews/', methods=['POST'])
+def get_reviews():
+    try:
+        # Call the controller function to get the list of reviews
+        reviews = list_review_log_json()
+
+        if reviews:
+            return jsonify(reviews), 200  # 200 OK status code for successful retrieval
+        else:
+            return jsonify({'error': 'No reviews found'}), 404  # 404 Not Found status code if no reviews found
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # 500 Internal Server Error for unexpected exceptions
--- App/views/student.py
+++ App/views/student.py
@@ -13,13 +13,13 @@ from App.controllers import (
 
 student_views = Blueprint('student_views', __name__, template_folder='../templates')
 
-@student_views.route('/student/add-student', methods=['POST'])
+@student_views.route('/student/<string:name>/<string:degree>/<int:year>/<int:karma>', methods=['POST'])
 def add_student_route():
     try:
         # Get JSON data from the request
-        data = request.get_json()
+        data = request.get_json(sname, degree, year, karma)
         # Call the controller function to add a student
-        success, message = add_student(data.get('studentName'), data.get('degree'), data.get('year'), data.get('karma'))
+        success, message = add_student(sname, degree, year, karma)
         if success:
             return jsonify({'message': message}), 201  # 201 Created status code for successful addition
         else:
@@ -51,14 +51,13 @@ def get_student_by_id_route(sid):
         return jsonify({'error': str(e)}), 500  # 500 Internal Server Error for unexpected exceptions
  
 
-@student_views.route('/student/get-students-by-name/<string:studentName>', methods=['GET'])
+@student_views.route('/student/<string:studentName>', methods=['GET'])
 def get_students_by_name_route(studentName):
     try:
-        # Get the 'sname' query parameter from the request
-        sname = request.args.get('studentName')
+        # 'studentName' is already captured by the route parameter
 
         # Call the controller function to get students by name
-        students = get_students_by_name(sname)
+        students = get_students_by_name(studentName)
 
         if students:
             # Convert the list of student objects to a list of dictionaries
@@ -105,15 +104,14 @@ def get_all_students_route():
     except Exception as e:
         return jsonify({'error': str(e)}), 500  # 500 Internal Server Error for unexpected exceptions
 
-# Update a student via POST request
-@student_views.route('/student/update-student/<int:sid>', methods=['PUT'])
-def update_student_route(sid):
+# Update a student via PUT request
+def update_student_route(id):
     try:
         # Get JSON data from the request
         data = request.get_json()
 
         # Call the controller function to update a student
-        success, message = update_student(sid, data.get('studentName'), data.get('degree'), data.get('year'), data.get('karma'))
+        success, message = update_student(id, data.get('studentName'), data.get('degree'), data.get('year'), data.get('karma'))
 
         if success:
             return jsonify({'message': message}), 200  # 200 OK status code for successful update
@@ -128,16 +126,13 @@ def delete_student_route(sid):
     try:
         # Call the controller function to delete a student
         success, message = delete_student(sid)
-
         if success:
             return jsonify({'message': message}), 200  # 200 OK status code for successful deletion
         else:
             return jsonify({'error': message}), 400  # 400 Bad Request status code for errors
-
     except Exception as e:
         return jsonify({'error': str(e)}), 500  # 500 Internal Server Error for unexpected exceptions
 
-# Update a student's karma via POST request
 @student_views.route('/student/update-karma/<int:sid>', methods=['PUT'])
 def update_karma_route(sid):
     try:
